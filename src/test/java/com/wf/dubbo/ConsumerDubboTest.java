2bf95be24e8edfd2b7669ac9527b8f8c1adfd764
wangpf <wangpf2011@163.com>
2018-11-18 17:31:07 +0800

    多协议多版本

--- src/test/java/com/wf/dubbo/ConsumerDubboTest.java
+++ src/test/java/com/wf/dubbo/ConsumerDubboTest.java
@@ -5,10 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ConsumerDubboTest {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer-p2p.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
         context.start();
-        UserServiceP2P userServiceP2P = (UserServiceP2P)context.getBean("userServiceP2P"); // 获取远程服务代理
-        String hello = userServiceP2P.sayHello("world"); // 执行远程方法
-        System.out.println( hello ); // 显示调用结果
+        UserService userService = (UserService)context.getBean("userService"); // 获取远程服务代理
+        UserRequest req = new UserRequest();
+        req.setName("wpf");
+        UserResponse res = userService.sayHello(req); // 执行远程方法
+        System.out.println(res.getData()); // 显示调用结果
+        
+        UserService userService2 = (UserService)context.getBean("userService2"); // 获取远程服务代理
+        UserResponse res2 = userService2.sayHello(req); // 执行远程方法
+        System.out.println(res2.getData()); // 显示调用结果
 	}
 }
