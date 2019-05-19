9cf0510770cd9f5eee3ed5d04900078ce30d7072
wangpf <wangpf2011@163.com>
2019-05-19 23:17:36 +0800

    rocketmq4.4.0

---
+++ src/main/java/com/wf/rocketmq/RocketMQConsumer.java
@@ -0,0 +1,38 @@
+package com.wf.rocketmq;
+
+import java.util.List;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class RocketMQConsumer {
+	public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("evcgjx_camera");
+         
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("101.132.32.50:9876");
+        
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("lnintmqtopic", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ src/main/java/com/wf/rocketmq/test/UserController.java
@@ -0,0 +1,30 @@
+package com.wf.rocketmq.test;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+	@Autowired
+	@Qualifier("rocketMQProducer")
+	private RocketMQProducer producer;
+ 
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+		for (int i = 0; i < 100; i++) {
+			Message msg = new Message("TestTopic1", "TAG1", (i + "這是spring集成").getBytes());
+			SendResult result = producer.getDefaultMQProducer().send(msg);
+			System.out.println(result);
+			System.out.println(1);
+		}
+		return "hello";
+	}
+}
